937c2ee2a01fbd51b18a958ac9c7aeaf79a140f7
Raghavender <raghavenderreddylonka.1@gmail.com>
2022-05-10 20:26:07 +0530

    change company name

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -7,27 +7,28 @@ export default function Testimonials() {
       <div class="testimonialsContainer">
         <div class="customerReview">
           <blockquote>
-            “Xometry has always given us exactly what we needed. We’ve received
-            parts with holes in the right places, with the right finish, and on
-            time. Their user-friendly website means faster turnaround.”
+            “Nidavellir has always given us exactly what we needed. We’ve
+            received parts with holes in the right places, with the right
+            finish, and on time. Their user-friendly website means faster
+            turnaround.”
           </blockquote>
           <cite>Jackson Siu</cite> <br />
           <small>Lead Mechanical Engineer, Asylon</small>
         </div>
         <div class="customerReview">
           <blockquote>
-            “Xometry delivers professionalism and ease. They take care of
+            “Nidavellir delivers professionalism and ease. They take care of
             fulfillment so I don't need to juggle or manage suppliers. We buy
-            the quote, and Xometry takes care of the rest.”
+            the quote, and Nidavellir takes care of the rest.”
           </blockquote>
           <cite>Ryan Lambert</cite> <br />
           <small>Tooling Design Engineer, BMW</small>
         </div>
         <div class="customerReview">
           <blockquote>
-            “I have been impressed with how forthcoming and responsive Xometry
-            has been in all of our communications. Your prices have won you a
-            lot of friends here at Thermaco.”
+            “I have been impressed with how forthcoming and responsive
+            Nidavellir has been in all of our communications. Your prices have
+            won you a lot of friends here at Thermaco.”
           </blockquote>
           <cite>Bruce Kyles</cite> <br />
           <small>Engineer, Thermaco</small>
